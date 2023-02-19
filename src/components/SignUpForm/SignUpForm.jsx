c41b5a9d33a68f2d9606a79b1d37cf82c7f9cfc1
Vadim Honcharuk <vadimhonc@gmail.com>
2023-02-19 17:18:51 +0200

    some new  inputs

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -12,6 +12,30 @@ export const SignUpForm = () => {
                         <input className={c.name} type="text" placeholder="Ім'я" name="firstname" required></input>
                         <input className={c.name} type="text" placeholder="Прізвище" name="secondname" required></input>
                     </div>
+                    <span className={c.label}>Дата народження студента<span className={c.neces}>&#9913;</span></span>
+                    <div>
+                        <input className={c.birth} type="text" placeholder="День" name="day" required></input>
+                        <select id="cars" name="cars" placeholder="Місяць">
+                            <option value="none" disabled selected>Місяць</option>
+                            <option value="january">Січень</option>
+                            <option value="february">Лютий</option>
+                            <option value="march">Березень</option>
+                            <option value="april">Квітень</option>
+                            <option value="may">Травень</option>
+                            <option value="june">Червень</option>
+                            <option value="july">Липень</option>
+                            <option value="august">Серпень</option>
+                            <option value="september">Вересень</option>
+                            <option value="october">Жовтень</option>
+                            <option value="november">Листопад</option>
+                            <option value="december">Грудень</option>
+                        </select>
+                        <input className={c.birth} type="text" placeholder="Рік" name="year" required></input>
+                    </div>
+                    <span className={c.label}>Номер телефону студента<span className={c.neces}>&#9913;</span></span>
+                    <div>
+                        <input className={c.name} type="text" placeholder="+38_________" name="firstname" required></input>
+                    </div>
                 </form>
             </div>
         </div>
